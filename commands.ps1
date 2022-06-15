476ac2b7c62c16240d9b2890f036ba3f9fba2a32
Ronald Pineda <ronystivens@gmail.com>
2022-06-16 00:55:44 +0200

    sending/getting info to/from files

--- commands.ps1
+++ commands.ps1
@@ -6,3 +6,31 @@ Get-Process | Where-Object {$_.name -eq "TextEdit"} | Stop-Process
 
 #get some process and sorting by id
 Get-Process -name textedit | sort-object -property id
+
+#send proccess data to a file
+Get-Process > process.txt
+
+#export it as csv file:
+Get-Process | export-csv proc.csv
+
+# o tmb
+Get-Process | Out-File proccess2.txt
+
+#see that content (like cat)
+Get-Content ./proccess.txt
+
+# export as xml:
+$procs2 = Get-Process | Export-Clixml proccess3.xml
+
+#Remoing that file
+Remove-Item ./proccess.txt
+
+#if I want to get objects from the data imported from a csv file:
+$procs = import-csv ./proc.csv
+
+#display that object ( i can check that with $procs | gm )
+$procs
+$procs2
+
+#sorting proccesses by descending working set and listing first 5:
+Get-Process | Sort-Object ws -Descending | Select-Object -first 5
